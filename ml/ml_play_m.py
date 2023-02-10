d034568c4226237604468acf740afda480213beb
JackHung <hungjack1222@gmial.com>
2023-02-10 10:59:22 +0800

    ref

--- ml/ml_play_m.py
+++ ml/ml_play_m.py
@@ -8,7 +8,7 @@ class MLPlay:
         Constructor
         """
         self.ball_served = False
-        self.loc = (93, 125)
+        self.loc = (93, 125) # 設置初始球的位置
 
     def update(self, scene_info):
         """
@@ -32,34 +32,28 @@ class MLPlay:
                 a = (self.loc[1] - scene_info["ball"][1]) / (self.loc[0] - scene_info["ball"][0])
                 b = scene_info["ball"][1] - (a * scene_info["ball"][0])
                 x = (393 - b) / a
+
                 if(x < 0 or x > 200):   #反摺
-                    x = (393 - b) * a * -1
+                    b = scene_info["ball"][1] - ((-1/a) * scene_info["ball"][0])
+                    x = (393 - b) * a * (-1)
             
-            if 0 < x < 200:
-                print(f"y = {a}x + {b}")
-                print(x)
-                print("                                ")
-            print(scene_info["ball"], " ", self.loc, " ", scene_info["platform"])
+            # if 0 < x < 200:  # debug
+            #     print(f"y = {a}x + {b}")
+            #     print(x)
+            #     print("                                ")
+            # print(scene_info["ball"], " ", self.loc, " ", scene_info["platform"])
 
             if (0 < x < 200):
-                if scene_info["platform"][0] + 15 < x:
+                if scene_info["platform"][0] + 20 < x:
                     command = "MOVE_RIGHT"
                 elif scene_info["platform"][0] + 20 > x:
                     command = "MOVE_LEFT"
                 else:
                     command = "NONE"
-            else:
-                command = "NONE"    #其他處理
-                if scene_info['platform'][0] > scene_info['ball'][0]:
-                    command = "MOVE_LEFT"
-
-                elif scene_info['platform'][0] < scene_info['ball'][0]:
-                    command = "MOVE_RIGHT"
-                else: 
-                    command = "NONE"
-
-
-        self.loc = scene_info["ball"]
+            else:   # 其他處理
+                command = "NONE"
+              
+        self.loc = scene_info["ball"] # 紀錄此幀的球位置，以便下幀的斜率計算
 
         return command
 
